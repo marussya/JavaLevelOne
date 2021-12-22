0805e342e8725e5df68d79a5ff51bed7a949df68
marussya <89848161+marussya@users.noreply.github.com>
2021-12-22 12:17:43 +0300

    HomeWork #4 fixed

--- lesson4/TicTacToe.java
+++ lesson4/TicTacToe.java
@@ -9,20 +9,27 @@ import java.util.Random;
 import java.util.Scanner;
 
 class TicTacToe {
-    final char SIGN_X = 'x'; //это константа и это поле
-    final char SIGN_O = 'o';
-    final char SIGN_EMPTY = '_';
+
+    final char SIGN_X = 'x';     //sign of human
+    final char SIGN_O = 'o';     //sign of AI
+    final char SIGN_EMPTY = '_'; //sign of empty cell
+    final int SIZE = 3;          //sign of the game table
+    final int WIN_SIZE = 3;      //sign of win-line
+    final String MSG_FOR_HUMAN = "Enter x y [1...3]: ";
+    final String MSG_YOU_WON = "YOU WON!";
+    final String MSG_AI_WON = "AI WON!";
+    final String MSG_DRAW = "Sorry, DRAW!";
+    final String MSG_GAME_OVER = "GAME OVER";
     char[][] table;
     Scanner sc;
     Random random;
     
     public static void main(String[] args) {
-        new TicTacToe().game(); //это особенный метод, который в ООП называется конструктор
+        new TicTacToe().game(); 
     }
     
     TicTacToe() { 
-        //конструктор: инициализация полей - имя конструктора должно совпадеть с именем класса
-        table = new char[3][3];
+        table = new char[SIZE][SIZE];
         sc = new Scanner(System.in);
         random = new Random();
     }
@@ -32,55 +39,56 @@ class TicTacToe {
         printTable();
         while (true) {
             turnHuman();
-            if (isWin(SIGN_X)) {
-                System.out.println("YOU WIN!");
+            if (checkWin(SIGN_X)) {
+                System.out.println(MSG_YOU_WON);
                 break;
             }
             if (isTableFull()) {
-                System.out.println("Sorry, DRAW!");
+                System.out.println(MSG_DRAW);
                 break;
             }
             turnAi();
             printTable();
-            if (isWin(SIGN_O)) {
-                System.out.println("AI WIN!");
+            if (checkWin(SIGN_O)) {
+                System.out.println(MSG_AI_WON);
                 break;
             }
             if (isTableFull()) {
-                System.out.println("Sorry, DRAW!");
+                System.out.println(MSG_DRAW);
                 break;
             }
         }
         printTable();
-        System.out.println("GAME OVER");
+        System.out.println(MSG_GAME_OVER);
     }
 
-    void initTable() { //инициализция таблицы
-        for (int y = 0; y < table.length; y++) 
-            for (int x = 0; x < table.length; x++) 
-                table[y][x] = SIGN_EMPTY;
+    void initTable() { 
+        for (int i = 0; i < SIZE; i++) 
+            for (int j = 0; j < SIZE; j++) 
+                table[i][j] = SIGN_EMPTY;
     }
     
-    void printTable() { //печать таблицы
-        for (int y = 0; y < table.length; y++) {
-            for (int x = 0; x < table.length; x++) {
-                System.out.print(table[y][x] + "|");
+    void printTable() { 
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(table[i][j] + "|");
             }
             System.out.println();
         }
+        System.out.println();
     }
     
-    void turnHuman() { //ход человека
+    void turnHuman() { 
         int x, y;
         do {
-            System.out.println("Enter x y [1...3]: ");
+            System.out.println(MSG_FOR_HUMAN);
             x = sc.nextInt() - 1;
             y = sc.nextInt() - 1;
         } while (!isCellValid(x, y));
         table[x][y] = SIGN_X;
     }
     
-    void turnAi() { //ход компьютера
+    void turnAi() { 
         int x, y;
         do {
             x = random.nextInt(3);
@@ -89,28 +97,29 @@ class TicTacToe {
         table[x][y] = SIGN_O;
     }
     
-    boolean isCellValid(int x, int y) { //валидна ли ячейка
+    boolean isCellValid(int x, int y) { 
         if (x < 0 || y < 0 || x > 2 || y > 2) {
             return false;
         }
         return table[x][y] == SIGN_EMPTY;
     }
     
-    boolean isWin(char ch) { //проверка на победу
-        //В ДЗ проверять только горизонталь и вертикаль через цикл 
-        for (int i = 0; i < table.length; i++) {
+    boolean checkWin(char ch) { 
+        for (int i = 0; i < SIZE; i++) {
             if ((table[i][0] == ch && table[i][1] == ch && table[i][2] == ch) || (table[0][i] == ch && table[1][i] == ch && table[2][i] == ch))
                 return true;
-            /*if ((table[0][0] == ch && table[1][1] == ch &&table[2][2] == ch) || (table[2][0] == ch && table[1][1] == ch &&table[0][2] == ch)) 
-                return true; //проверка на диагональ */
         }
-    return false;
+        if ((table[0][0] == ch && table[1][1] == ch &&table[2][2] == ch) || 
+            (table[2][0] == ch && table[1][1] == ch &&table[0][2] == ch)) {
+            return true;
+            }
+        return false;
     }
     
-    boolean isTableFull() { //проверка на ничью
-        for (int y = 0; y < table.length; y++) {
-            for (int x = 0; x < table.length; x++) {
-                if (table[y][x] == SIGN_EMPTY) {
+    boolean isTableFull() { 
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (table[i][j] == SIGN_EMPTY) {
                     return false;
                 }
             }
