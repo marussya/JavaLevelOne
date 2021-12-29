f318a9af47c0e01712a05a86f9884e71ea70d3f7
marussya <89848161+marussya@users.noreply.github.com>
2021-12-29 22:36:13 +0300

    add HelloSwing.java

---
+++ Swing/HelloSwing.java
@@ -0,0 +1,22 @@
+import javax.swing.JFrame;
+import java.awt.Button;
+import java.awt.BorderLayout;
+
+class HelloSwing extends JFrame {
+    public static void main(String[] args) {
+        new HelloSwing();
+    }
+    
+    HelloSwing() {
+        setTitle("hello Swing!");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(500, 250);
+        setLocationRelativeTo(null);
+        
+        Button btnExit = new Button("Exit");
+        btnExit.addActionListener(e -> System.exit(0));
+        
+        add(btnExit, BorderLayout.SOUTH);
+        setVisible(true);
+    }
+}
